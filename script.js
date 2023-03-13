7236c8f55cbabc0825e0227e8709300648820257
Arthur Araujo <arthur.araujo@ceuma.br>
2023-03-13 11:23:41 -0300

    atualizando relogio pra nicolle

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ const relogio = setInterval(function time() {
   
   if (6 <= hr && hr < 12) {
     document.body.style.background =
-      "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+      "linear-gradient(216deg, rgba(34,193,195,1) 26%, rgba(126,190,132,1) 52%, rgba(206,188,77,1) 66%, rgba(253,187,45,1) 85%, rgba(253,187,45,1) 96%)";
   } else if (12 <= hr && hr < 18) {
     document.body.style.background =
-    "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+    "linear-gradient(52deg, rgba(156,0,0,1) 0%, rgba(212,55,28,1) 19%, rgba(219,103,25,1) 38%, rgba(125,119,113,1) 62%, rgba(0,146,212,1) 93%)";
   } else {
     document.body.style.background =
-    "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+    "linear-gradient(52deg, rgba(184,86,0,1) 0%, rgba(128,34,3,1) 26%, rgba(22,53,73,1) 46%, rgba(16,30,82,1) 66%, rgba(1,0,34,1) 86%)";
   }
 });
 
